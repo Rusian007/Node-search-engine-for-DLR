b6dc8d00b78e9bdeb253ef960d289bdfde5934bc
Arafat <arafat.rahman19@northsouth.edu>
2022-10-23 00:21:49 +0600

    Python files removed

--- pyscripts/filter.py
+++
@@ -1,6 +0,0 @@
-import sparker
-
-def FilterText():
-    filtered = sparker.df.filter(lambda ele: "twinkle" in ele)
-    filter = filtered.collect()
-    return filter
--- venv/lib/python3.10/base64.py
+++
@@ -1,1 +0,0 @@
-/usr/lib/python3.10/base64.py
